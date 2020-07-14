531817d868bfc039f6b1b2daa5279ab16dc18104
Haruka0519 <luka000519@gmail.com>
2020-07-14 14:07:47 +0900

    Update Program.cs

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("うわぁぁーん");
+
         }
     }
 }
