1d8a1e8003a468d9c680119771711b4ac04e96b0
Haruka0519 <luka000519@gmail.com>
2020-07-14 15:57:34 +0900

    ｐｔ

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,10 +6,47 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string exheight = "";
+            string exweight = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i == 0) exheight = args[i];
+                if (i == 0) exweight = args[i];
 
-            Console.WriteLine("うわぁぁーん");
+            }
 
+            float height = EX("身長を入力してください(m)");
+            float weight = EX("体重を入力してください(kg)");
+            float bmi = BMI(height, weight);
+
+            Console.WriteLine("BMIは" + bmi + "です");
         }
+        static float EX(string top, string log)
+        {
+            float PV = 0f;
+            bool PS = float.TryParse(top, out PV);
+           
+            while (PS)
+            {
+                Console.WriteLine(log);
+                string input = Console.ReadLine();
+                PS = float.TryParse(input, out PV);
+
+                if (PS)
+                    Console.WriteLine("数値を入力してください");
+            }
+            return PV;
+        }
+        static float BMI(float height,float weight)
+        {
+            return weight / (height + height);
+        }
+    
+    
+    
+    
+    
+    
+    
     }
 }
